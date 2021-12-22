13e37adad12f9cacb269914d12fa63fc5253060a
Isaiah Lleva <isaiahlleva@gmail.com>
2021-12-22 07:46:56 -0600

    Create a blur on image when clicked

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 let photo = document.getElementById('about-me-photo');
 
 let blur = () => {
-    photo.style.border = '5px dashed #64ffda';
+    photo.style.opacity = '0.5';
 };
 
 let unblur = () => {
-    photo.style.border = '5px solid #64ffda';
+    photo.style.opacity = '1';
 };
 
-photo.addEventListener('mouseover', blur);
-photo.addEventListener('mouseout', unblur);
+photo.addEventListener('mousedown', blur);
+photo.addEventListener('mouseup', unblur);
