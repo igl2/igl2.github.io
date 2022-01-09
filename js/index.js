3b10d6259b01f824cb6d59ca0436d459becf67a5
Isaiah Lleva <isaiahlleva@gmail.com>
2022-01-09 17:16:03 -0600

    Adding extra features for interactability

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 let photo = document.getElementById('online-fit-expert-photo');
+let details = document.getElementById('click-on-img-text');
 
 let blur = () => {
     photo.style.opacity = '0.5';
+    details.style.display = 'block';
 };
 
 let unblur = () => {
     photo.style.opacity = '1';
+    details.style.display = 'none';
 };
 
 photo.addEventListener('mouseover', blur);
