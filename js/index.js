bb6242a6d6de292129c691b75b95c0141d683297
Isaiah Lleva <isaiahlleva@gmail.com>
2022-09-14 21:22:10 -0500

    Add date feature in hero

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 let currentYear = new Date().getFullYear();
+let currentDate = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) ;
+
 $('#year').text(currentYear);
+$('#date').text(currentDate);
 
 $(window).scroll(function(){
 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
